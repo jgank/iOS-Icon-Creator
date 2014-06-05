570f6aaa2149961052e482070a4ecb467d4f8c12
Justin Knag <justinknag@gmail.com>
2014-06-04 23:42:55 -0400

    second commit

---
+++ iosicons.sh
@@ -0,0 +1,30 @@
+mv *.png 1024.png
+cp 1024.png 57.png
+cp 1024.png 114.png
+cp 1024.png 120.png
+cp 1024.png 72.png
+cp 1024.png 144.png
+cp 1024.png 76.png
+cp 1024.png 152.png
+cp 1024.png 29.png
+cp 1024.png 58.png
+cp 1024.png 80.png
+cp 1024.png 50.png
+cp 1024.png 100.png
+cp 1024.png 40.png
+cp 1024.png 80.png
+
+sips -Z 57 57.png
+sips -Z 114 114.png
+sips -Z 120 120.png
+sips -Z 72 72.png
+sips -Z 144 144.png
+sips -Z 76 76.png
+sips -Z 152 152.png
+sips -Z 29 29.png
+sips -Z 58 58.png
+sips -Z 80 80.png
+sips -Z 50 50.png
+sips -Z 100 100.png
+sips -Z 40 40.png
+sips -Z 80 80.png

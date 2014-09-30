af1c7d13710decf03b5f4353e7267c0af4f406a6
Justin Knag <justinknag@gmail.com>
2014-09-30 03:10:50 -0400

    updated sizes

--- iosicons.sh
+++ iosicons.sh
@@ -1,10 +1,22 @@
 mv *.png 1024.png
+cp 1024.png 29.png
+cp 1024.png 40.png
+cp 1024.png 58.png
+cp 1024.png 60.png
+cp 1024.png 80.png
 cp 1024.png 120.png
 cp 1024.png 152.png
 cp 1024.png 76.png
+cp 1024.png 120.png
 cp 1024.png 180.png
 
+sips -Z 29 29.png
+sips -Z 40 40.png
+sips -Z 58 58.png
+sips -Z 60 60.png
+sips -Z 80 80.png
 sips -Z 120 120.png
 sips -Z 152 152.png
 sips -Z 76 76.png
+sips -Z 120 120.png
 sips -Z 180 180.png
